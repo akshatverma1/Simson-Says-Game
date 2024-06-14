bbd604161f5ec14856da834810df2b800dd2d11d
akshatverma1 <akshaverma14@gmail.com>
2024-06-14 23:44:53 +0530

    14/06/2024

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
-let btn = document.querySelector(".block");
-btn.addEventListener("click",function(event){
-    console.log("Button Clicked!!");
+let gameSeg = [];
+let userSeq = [];
+let level = 0;
+let started = false;
+
+
+let body = document.querySelector("body");
+let heading = document.querySelector("h3");
+
+function flash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },1000);
+}
+function levelUp(){
+    level++;
+    heading.innerText=`Level = ${level}`;
+    flash();
+}
+
+body.addEventListener("keypress", function () {
+    if (started == false) {
+        heading.innerText = "Game Start";
+        started = true;
+        console.log("Game Started");
+        levelUp();
+    }
 })
