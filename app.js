b5b2f18c66a5547641db41eed2106ebd883bfd8d
akshatverma1 <akshaverma14@gmail.com>
2024-06-15 01:15:35 +0530

    15/06/2024

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-let gameSeg = [];
+let gameSeq = [];
 let userSeq = [];
 let level = 0;
 let started = false;
-
+let btn = ["yellow","red","purple","green"];
 
 let body = document.querySelector("body");
 let heading = document.querySelector("h3");
 
-function flash(btn){
-    btn.classList.add("flash");
+function flash(btnm){
+    btnm.classList.add("flash");
     setTimeout(function(){
-        btn.classList.remove("flash");
-    },1000);
+        btnm.classList.remove("flash");
+    },200);
 }
 function levelUp(){
+    userSeq =[];
     level++;
     heading.innerText=`Level = ${level}`;
-    flash();
+    let random = Math.floor(Math.random()*3);
+    let randombtn = btn[random];
+    let randomstr = document.querySelector(`.${randombtn}`); 
+    console.log(randombtn);
+    flash(randomstr);
+    gameSeq.push(randombtn);
+    console.log(gameSeq);
 }
 
 body.addEventListener("keypress", function () {
@@ -27,3 +34,37 @@ body.addEventListener("keypress", function () {
         levelUp();
     }
 })
+
+function checkAns(){
+    let inx =level-1;
+    if(userSeq[inx]===gameSeq[inx]){
+        if(userSeq.length==gameSeq.length){
+            levelUp();
+        }
+        console.log("succes");
+    }else{
+        heading.innerText="Game Over";
+        console.log("Better Luck Next Time");
+        reset();
+    }
+}
+function buttonPress(){
+    console.log("Button Press");
+    let fl = this;
+    flash(fl);
+    idd = fl.getAttribute("id");
+    userSeq.push(idd);
+    console.log(userSeq)
+    checkAns();
+}
+let allbtn = document.querySelectorAll(".block");
+for(nnn of allbtn){
+    nnn.addEventListener("click",buttonPress);
+}
+function reset(){
+    let gameSeq = [];
+let userSeq = [];
+let level = 0;
+let started = false;
+console.log("reset invoked");
+}
